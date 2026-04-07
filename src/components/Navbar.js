c12e337a1d41de946db1e52ebd386171ab4cd5d8
kaushiki <kaushiki@labdox.in>
2026-04-07 17:32:21 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,12 @@
+import { auth } from './db/firebase-config.js';
+import { signOut } from "firebase/auth";
 
+window.handleLogout = async () => {
+    try {
+        await signOut(auth);
+        localStorage.clear(); // Important: Clear teamId and role!
+        window.location.href = "index.html";
+    } catch (error) {
+        console.error("Logout failed", error);
+    }
+};
