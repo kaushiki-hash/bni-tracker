eeb6108d8c99f672fb18186ec95d97c1196ca519
kaushiki <kaushiki@labdox.in>
2026-04-07 14:20:21 +0530

    Update firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:12345:web:abc123"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
