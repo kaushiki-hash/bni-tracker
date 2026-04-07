eae6a2caa5a729d70586580d087bb36688fca693
kaushiki <kaushiki@labdox.in>
2026-04-07 17:08:11 +0530

    Update firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,16 +1,21 @@
+// src/db/firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: ".firebaseapp.com",
-  projectId: "-app",
-  storageBucket: ".appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:12345:web:abc123"
+  apiKey: "AIzaSyDINqAKeeoVU8KRhvYc_c9PMkYCkR07Z1I",
+  authDomain: "bniattendance-96205.firebaseapp.com",
+  projectId: "bniattendance-96205",
+  storageBucket: "bniattendance-96205.firebasestorage.app",
+  messagingSenderId: "250805340036",
+  appId: "1:250805340036:web:0eaf64fcecc66535a27fd1",
+  measurementId: "G-NY8CV4X36R"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+//  can use them in code
 export const db = getFirestore(app);
 export const auth = getAuth(app);
