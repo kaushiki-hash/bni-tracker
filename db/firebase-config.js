002c0f6cf97178f75cb4816787971903d6115b28
kaushiki <kaushiki@labdox.in>
2026-04-09 12:36:08 +0530

    Update firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,7 +1,9 @@
-﻿import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+// Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+// 2nd acc Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDINqAKeeoVU8KRhvYc_c9PMkYCkR07Z1I",
   authDomain: "bniattendance-96205.firebaseapp.com",
@@ -12,6 +14,9 @@ const firebaseConfig = {
   measurementId: "G-NY8CV4X36R"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export instances to use in auth.js and app.js
 export const db = getFirestore(app);
 export const auth = getAuth(app);
